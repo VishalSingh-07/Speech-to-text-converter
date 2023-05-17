0ec0dfbfccd7b0277c3bbaafe2071edb66f7a8d0
VishalSingh-07 <vishalsinghagr25@gmail.com>
2023-05-17 15:49:15 +0530

    Github Deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Speech-to-text-converter",
   plugins: [react()],
-  base: "/Speech-to-text-converter/",
 });
